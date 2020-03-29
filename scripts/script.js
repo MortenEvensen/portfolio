1852b45f1b4b2789f8233e544592d1f2df8b002f
MortenEvensen <evensenmorten@hotmail.com>
2020-03-29 22:00:50 +0200

    Finished for delivery

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,22 @@
-/* Open when someone clicks on the span element */
-function openNav() {
-  document.getElementById("overlay-menu").style.width = "100%";
-}
-
-/* Close when someone clicks on the "x" symbol inside the overlay */
-function closeNav() {
-  document.getElementById("overlay-menu").style.width = "0%";
-}
-
-
-
 /* Open and close fullscreen size windows on index page */
-var leftStyle = document.querySelector(".left");
-var leftResult = getComputedStyle(leftStyle);
-var rightStyle = document.querySelector(".right");
-var rightResult = getComputedStyle(rightStyle);
+var leftStyle = document.getElementById("left");
+var rightStyle = document.getElementById("right");
 
-var leftImage = document.querySelector(".coding-image")
-var imagewrap = document.querySelector(".firstcontentwrap")
-var imageWrapResult = getComputedStyle(imagewrap)
+var imagewrap = document.getElementById("firstcontentwrap")
 
-var header = document.querySelector(".header");
-var headerResult = getComputedStyle(header);
-var webHeader = document.querySelector(".web");
-var musicHeader = document.querySelector(".music");
+var header = document.getElementById("header");
+
+var webHeader = document.getElementById("web");
+var musicHeader = document.getElementById("music");
+
 var webText = document.getElementById("web-text");
 var webButton = document.getElementById("web-button");
-var musicText = document.getElementById("music-text"); 
+
+var musicText = document.getElementById("music-text");
 var musicButton = document.getElementById("music-button");
 
 leftStyle.addEventListener("click", function() {
-    if (/*leftResult.flex === "1 1 0%" && rightResult.flex === "1 1 0%" && window.innerWidth >= 800 || */leftStyle.classList.contains("flex") === false && window.innerWidth >= 800) {
+    if (leftStyle.classList.contains("flex") === false && window.innerWidth >= 800) {
         rightStyle.style.flex = 0;
         rightStyle.style.width = 0;
         imagewrap.style.height = "140vh"
@@ -41,14 +27,12 @@ leftStyle.addEventListener("click", function() {
         musicHeader.style.display = "none";
         webText.style.opacity = "1";
         webButton.style.opacity = "1";
-        //webText.style.display = "block"
-        //webButton.style.display = "block"
         musicButton.style.display = "none";
         musicText.style.display = "none"
         webText.style.marginLeft = "20px";
         webButton.style.marginLeft = "0px";
-        
-    } else if (leftStyle.classList.contains("flex") === true && window.innerWidth >= 800)  {
+
+    } else if (leftStyle.classList.contains("flex") === true && window.innerWidth >= 800) {
         rightStyle.style.flex = 1;
         rightStyle.style.width = "100%";
         imagewrap.style.height = "100vh";
@@ -59,49 +43,41 @@ leftStyle.addEventListener("click", function() {
         musicHeader.style.display = "block"
         webText.style.opacity = 0
         webButton.style.opacity = 0
-        //webText.style.display = "none"
-        //webButton.style.display = "none"
         musicButton.style.display = "block";
         musicText.style.display = "block"
-        webText.style.marginLeft = "-1000px";
-        webButton.style.marginLeft = "-1000px";
-        
+        webText.style.marginLeft = "-3000px";
+        webButton.style.marginLeft = "-3000px";
+
     } else if (leftStyle.classList.contains("flex") === false && window.innerWidth < 800) {
         rightStyle.style.flex = 0;
         rightStyle.style.height = 0;
         musicHeader.style.display = "none";
-        //imagewrap.style.height = "100vh"
         leftStyle.classList.add("flex");
         webText.style.opacity = 1
         webButton.style.opacity = 1
-         musicButton.style.display = "none"
+        musicButton.style.display = "none"
         musicText.style.display = "none"
-       // webText.style.display = "block"
-       // webButton.style.display = "block"
         webText.style.marginLeft = "20px";
         webButton.style.marginLeft = "0px";
-    } else if (leftStyle.classList.contains("flex") === true && window.innerWidth < 800)  {
+    } else if (leftStyle.classList.contains("flex") === true && window.innerWidth < 800) {
         rightStyle.style.flex = 1;
         rightStyle.style.width = "100%";
-       // imagewrap.style.height = "100vh"; 
         leftStyle.classList.remove("flex");
         musicHeader.style.display = "block";
         webText.style.opacity = 0
         webButton.style.opacity = 0
-         musicButton.style.display = "block"
+        musicButton.style.display = "block"
         musicText.style.display = "block"
-       // webText.style.display = "none"
-      //  webButton.style.display = "none"
-        webText.style.marginLeft = "-1000px";
-        webButton.style.marginLeft = "-1000px";
-               }
+        webText.style.marginLeft = "-3000px";
+        webButton.style.marginLeft = "-3000px";
+    }
 })
 
 
 rightStyle.addEventListener("click", function() {
-    if (/*rightResult.flex === "1 1 0%"   && leftResult.flex === "1 1 0%" && window.innerWidth >= 800 || */rightStyle.classList.contains("flex") === false && window.innerWidth >= 800) {
+    if (rightStyle.classList.contains("flex") === false && window.innerWidth >= 800) {
         leftStyle.style.flex = 0;
-        leftStyle.style.width = 0; 
+        leftStyle.style.width = 0;
         imagewrap.style.height = "140vh"
         imagewrap.style.margin = "-40vh 0 0 0"
         header.style.margin = "40vh 0 0 0"
@@ -110,12 +86,10 @@ rightStyle.addEventListener("click", function() {
         webHeader.style.display = "none"
         musicText.style.opacity = 1
         musicButton.style.opacity = 1
-      //   musicButton.style.display = "block";
-      //  musicText.style.display = "block"
         musicText.style.marginLeft = "20px";
         musicButton.style.marginLeft = "0px";
-        
-        
+
+
     } else if (rightStyle.classList.contains("flex") === true && window.innerWidth >= 800) {
         leftStyle.style.flex = 1;
         leftStyle.style.width = "100%";
@@ -125,44 +99,41 @@ rightStyle.addEventListener("click", function() {
         musicHeader.style.top = "100px"
         rightStyle.classList.remove("flex")
         webHeader.style.display = "block"
-         musicText.style.opacity = 0
+        musicText.style.opacity = 0
         musicButton.style.opacity = 0
-       // musicButton.style.display = "none";
-       // musicText.style.display = "none"
-        musicText.style.marginLeft = "-1000px";
-        musicButton.style.marginLeft = "-1000px";
+        musicText.style.marginLeft = "-3000px";
+        musicButton.style.marginLeft = "-3000px";
 
     } else if (rightStyle.classList.contains("flex") === false && window.innerWidth < 800) {
         leftStyle.style.flex = 0;
-        leftStyle.style.height = 0; 
-        //imagewrap.style.height = "100vh"
-        
-        //imagewrap.style.margin = "0 0 0 0"
+        leftStyle.style.height = 0;
         header.style.margin = "0 0 0 0"
-        //musicHeader.style.top = "20vh"
         rightStyle.classList.add("flex")
-        webHeader.style.display = "none" 
-         musicText.style.opacity = 1
+        webHeader.style.display = "none"
+        musicText.style.opacity = 1
         musicButton.style.opacity = 1
-       //  musicButton.style.display = "block"
-      //  musicText.style.display = "block"
         musicText.style.marginLeft = "20px";
-        
         musicButton.style.marginLeft = "0px";
-    } else if (rightStyle.classList.contains("flex") === true && window.innerWidth < 800)  {
+    } else if (rightStyle.classList.contains("flex") === true && window.innerWidth < 800) {
         leftStyle.style.flex = 1;
         leftStyle.style.width = "100%";
-        //imagewrap.style.height = "100vh";
-        //imagewrap.style.margin = "0 0 0 0"
         header.style.margin = 0
         musicHeader.style.top = "100px"
         rightStyle.classList.remove("flex")
-        webHeader.style.display = "block" 
+        webHeader.style.display = "block"
         musicText.style.opacity = 0
         musicButton.style.opacity = 0
-      //  musicButton.style.display = "none"
-      //  musicText.style.display = "none"
-        musicText.style.marginLeft = "-1000px";
+        musicText.style.marginLeft = "-3000px";
+        musicButton.style.marginLeft = "-3000px";
     }
 })
 
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("overlay-menu").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("overlay-menu").style.width = "0%";
+}
